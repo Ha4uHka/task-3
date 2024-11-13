e2023eafd7ddd04024f6fbbd9f82372aab22980b
Ha4uHka <zhara1234567@mail.ru>
2024-11-13 13:34:54 +0300

    Добавил что-то

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 ## functions - модуль для импорта функций для работы
 
 # TODO import <зависимость из задания>
+# TODO переименовать функции
+from functions import funk1, funk2
 
 def funk1(n):
     """Функция 1"""
--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 
 # todo import <зависимость из задания>
 
-def main()
+def main():
     # TODO - сделай вызов функций из functions
     pass
 
 # Иннициализационный скрипт
 if __name__ == '__main__':
     main()
-    
