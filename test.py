46179fa823ddcc4ec7111eb1359bc4fdaa4fab95
Ha4uHka <zhara1234567@mail.ru>
2024-12-10 12:59:33 +0300

    Полурабочий Вариант

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
 
 #print(os.path.isfile('C:\\Users\\user\\envs\\task-3/data/A\\3'))
-print(os.path.isfile('C:/Users/user/envs/task-3/data'))
+#print(os.path.isfile('C:/Users/user/envs/task-3/data'))
 print(os.path.isfile("C:/Users/user/envs/task-3/data/A/1/1.jpg"))
